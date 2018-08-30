bc438ad7239b443f7d5af6bafeaf6997e4391029
huarui <huarui.wu@gmail.com>
2018-08-29 17:18:38 -0700

    move utils

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 
 import './App.css'
-import './approve'
-import { setBackground } from './background'
+import './utils/approve'
+import { setBackground } from './utils/background'
 
 setBackground('orbital insight')
 
