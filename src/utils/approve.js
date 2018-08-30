3c29a4b86de40bc108ce32c868efa81bb47698fb
huarui <huarui.wu@gmail.com>
2018-08-29 18:16:16 -0700

    tested create

--- src/utils/approve.js
+++ src/utils/approve.js
@@ -2,9 +2,17 @@ const Octokat = require('octokat')
 
 const octo = new Octokat({ token: process.env.REACT_APP_APPROVE_ME_TOKEN })
 
-// const repo = octo.repos('orbitalinsight', 'frontend')
-// const pull = repo.pulls(172)
-// pull.reviews.create()
-// pull.reviews.fetch().then(console.log)
+const repo = octo.repos('orbitalinsight', 'frontend')
+const pull = repo.pulls(172)
+// pull.reviews
+//     .create({
+//         body: 'test',
+//         event: 'APPROVE'
+//     })
+//     .then(console.log)
+pull.reviews.fetch().then(console.log)
+// pull.reviews(150808372)
+//     .remove()
+//     .then(console.log)
 
 export default octo
