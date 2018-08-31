9a553aa7b312ad7f0af72e67c09855aa2ec7c365
huarui <huarui.wu@gmail.com>
2018-08-30 22:41:29 -0700

    use new thumbnails

--- src/utils/thumbnail.js
+++ src/utils/thumbnail.js
@@ -3,9 +3,15 @@ import thumbnail2 from '../images/thumbsup2.jpg'
 import thumbnail3 from '../images/thumbsup3.jpg'
 import thumbnail4 from '../images/thumbsup4.jpg'
 import thumbnail5 from '../images/thumbsup5.jpg'
+import thumbnail6 from '../images/thumbsup6.jpg'
+import thumbnail7 from '../images/thumbsup7.jpg'
+import thumbnail8 from '../images/thumbsup8.jpg'
+import thumbnail9 from '../images/thumbsup9.jpg'
+
+const NUM_THUMBNAILS = 9
 
 const getRandomThumbsUp = () => {
-    const thumbnailNumber = Math.round(Math.random() * (5 - 1) + 1)
+    const thumbnailNumber = Math.round(Math.random() * (NUM_THUMBNAILS - 1) + 1)
     switch (thumbnailNumber) {
         case 1:
             return thumbnail1
@@ -17,6 +23,14 @@ const getRandomThumbsUp = () => {
             return thumbnail4
         case 5:
             return thumbnail5
+        case 6:
+            return thumbnail6
+        case 7:
+            return thumbnail7
+        case 8:
+            return thumbnail8
+        case 9:
+            return thumbnail9
     }
 }
 
